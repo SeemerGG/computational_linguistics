2064eb8947d679324851b302e2dc3130900a527f
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2022-10-03 13:50:04 +0300

    Лабораторная работа №1

--- Lab_1/Lab_1/Lab_1.py
+++ Lab_1/Lab_1/Lab_1.py
@@ -1,45 +1,45 @@
 from nltk import *
 from nltk.corpus import stopwords
 import docx 
-
-#вывод из файла .docx
-data_docx = docx.Document('input.docx')
-
-#считывание данных из файла в data
-data = []
-
-for paragraph in data_docx.paragraphs:
-    data.append(paragraph.text)
-data = ' '.join(data)
+import string
 
 
-##Пример 1. разделение входного текста на предложения 
+def input_doxc():
+    data_docx = docx.Document('input.docx')
+    data = []
+    for paragraph in data_docx.paragraphs:
+        data.append(paragraph.text)
+    data = ' '.join(data)
+    return data
 
-#sent = sent_tokenize(data)
-#print(sent)
+def graphematic_analiz(data):
+    
+    list_sent = sent_tokenize(data) #Список предложений
 
-##Пример 2. Разделение текста на слова 
+    data_without_znaks = data.lower()
+    for s in string.punctuation:
+        if s in data_without_znaks:
+            data_without_znaks = data_without_znaks.replace(s,'') #Строка без знаков препинания 
+    list_words = word_tokenize(data_without_znaks)
+    download('stopwords')
+    list_stop_word = set(stopwords.words('russian'))
+    list_words = [i for i in list_words if i not in list_stop_word]
+    #list_words - список без стоп слов и знаков + все в нижнем регистре
 
-#sent2 = word_tokenize(data)
+    #Создаю словарь где ключем является частота встречаемости слова
+    dict_frequency = {}
+    for i in list_words:
+        if i in dict_frequency:
+            dict_frequency[i] = dict_frequency[i]+1
+        else:
+            dict_frequency[i] = 1
+    return [list_sent, list_words, dict(sorted(dict_frequency.items(), key = lambda x: x[1], reverse = True))]
 
-#print (sent2)
+graph_analiz = graphematic_analiz(input_doxc())
 
-##Пример 3. Скачивание списка стоп слов (был произведен в консоли отдельно)
-##from nltk import download
-##download('stopwords')
+print(graph_analiz[0])
+print("\n")
+print(graph_analiz[1])
+print('\n')
+print(graph_analiz[2])
 
-##Пример 4. Просмотр стоп-слов
-
-#print(stopwords.words('russian'))
-
-##Пример 5.Удаление стоп-слов из предложения 
-
-
-#stop_words = set(stopwords.words('russian'))
-#sent2_without_stop_words = [word for word in sent2 if not word in stop_words]
-
-#print(sent2_without_stop_words)
-
-#Разработка графематического анализатора
-
-sent = sent_tokenize(data)
