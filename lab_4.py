d180d29e2c8d1d66061ea1d7fc7ffed31ef7d293
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2022-11-29 23:34:36 +0300

    1

---
+++ lab_4.py
@@ -0,0 +1,12 @@
+﻿import pymorphy2 as pm
+from nltk impotr word_tokenize
+
+def freq_semantic(fragment_1, fragment_2):
+	m = pm.MorphAnalyzer()
+	word_1 = set([m.parse(word)[0].normal_form for word in word_tokenize(fragment_1)])
+	return len(words_1 & wprds_2) / len(words_1)
+	
+q = input("Первый фрагмент: ")
+t = input("Второй фрагмент: ")
+
+print(freq_semantic(q, t))
---
+++ sint_anal.py
@@ -0,0 +1,21 @@
+﻿from natasha import * 
+
+segmenter = Segmenter()
+emb = NewsEmbedding()
+morph_targger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
+
+print("Текст для анализа:")
+text = input()
+
+doc = Doc(text)
+
+doc.segment(segmenter)
+doc.tag_morph(morph_targger)
+doc.parse_syntax(syntax_parser)
+
+for i in doc.sents:
+	i.syntax.print()
+
+"""Уважаемые дамы и  господа!  Мне  неизвестно, насколько каждый из вас из литературы или понаслышке знаком  с психоанализом. Однако само название моих лекций   "Элементарное  введение в  психоанализ" -  предполагает, что  вы ничего не знаете об этом и готовы получить от меня первые сведения."""
+
